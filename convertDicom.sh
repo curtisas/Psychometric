039f1d7a561db970a909dfff150a261ba5a019dc
Kevin D McGovney <kdmvff@mail.missouri.edu>
2019-02-27 16:00:07 -0600

    Updated converted Dicom file to work with SPIN2 data

--- convertDicom.sh
+++ convertDicom.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/bin/bash
 
 ###############################
 #
@@ -10,53 +9,47 @@
 #		USAGE:			CONVERT RAW DCM FILES TO NII FORMAT
 #
 #		MODIFIED ON:		2017_10_19
-#  	        MODIFIED ON:	        2017_12_06
-#               MODIFIED ON:            2019_02_20    BY Ashley Curtis (and friends)
+#  	MODIFIED ON:    2017_12_06
+#   MODIFIED ON:    2019_02_20    BY Ashley Curtis (and friends)
 #
 ###############################
 #
+# Default subject naming convention
+if [ -z $1 ] ; then 
+  sub='SPIN2_xxx'
+else
+  sub=$1
+fi
+# Default visit naming convention
+if [ -z $2 ] ; then 
+  visit='v1'
+else
+  visit=$2
+fi
+# Default condition naming convention
+if [ -z $3 ] ; then 
+  cond='unknown'
+else
+  cond=$3
+fi
+# Set the input and output directories
+# Example input file: SPIN2_001_v1
 
-sub="Sub031"
-visit="_v1"
-cond="_pos"
+dicomData=/storage/hpc/group/sleeplab/raw/${sub}_${visit}/
+niixOutput=/storage/hpc/group/sleeplab/preprocessed/${sub}_${visit}_${cond}/
 
-dicomData=/Volumes/Data/Imaging/R01/rawdata/${sub}${visit}"/"
-niixOutput=/Volumes/Data/Imaging/R01/preprocessed/_Jason_0/${sub}${visit}${cond}"/"
+#Create the desired output folder
+mkdir $niixOutput
 
-dcm2niix -b y -z y -o ${niixOutput} -f ${sub}_%p_s%2s ${dicomData}
-
+#Converts the Dicom to Nifti
+# %p=protocol %s=series number
+/storage/hpc/group/sleeplab/software/dcm2niix-1.0.20181125/bin/dcm2niix -b y -z y -o ${niixOutput} -f ${sub}_%p_s%2s ${dicomData}
 
 echo ${dicomData}
 echo ${niixOutput}
 cd ${niixOutput}
 
-#pwd
-#			RENAME OUTPUT FILES
-# mv ${sub}"_t1_"*".json"  "T1"${visit}".json"
-# mv ${sub}"_resting_bold1_"*".json"  "RSrun1"${visit}".json"
-# mv ${sub}"_resting_bold2_"*".json"  "RSrun2"${visit}".json"
-# mv ${sub}"_resting_bold3_"*".json"  "RSrun3"${visit}".json"
-# mv ${sub}"_resting_bold4_"*".json"  "RSrun4"${visit}".json"
-
-
-mv ${sub}"_t1_"*".gz"  "T1"${visit}".gz"
-mv ${sub}"_resting_bold1_"*".gz"  "RSrun1"${visit}".gz"
-mv ${sub}"_resting_bold2_"*".gz"  "RSrun2"${visit}".gz"
-mv ${sub}"_resting_bold3_"*".gz"  "RSrun3"${visit}".gz"
-mv ${sub}"_resting_bold4_"*".gz"  "RSrun4"${visit}".gz"
-
-
 #		CREATE NEW DIRECTORY AND MOVE EXTRA FILES
-mkdir AdditionalScans
-mv ${sub}* ./AdditionalScans
+mkdir -p metadata
+mv *.json ./metadata
 
-
-
-#mv ${niixOutput}"/"${sub}"_resting_bold2_"*".json"  "RSrun1"${visit}".json"
-#RSrun1_v1
-
-
-#			EXAMPLE FROM MRIroGL
-#dcm2niix" -b y -z y -f "%p"  "MyDicomFolder"
-#			EXAMPLE FROM PAUL WRIGHT
-#dcm2niix -m y -z y -o ${niftidirectory} -f SubID001_%t_s%s_%d ${dicomdirectory}
